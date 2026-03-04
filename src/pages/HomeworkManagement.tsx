c498cfdbb33be8001491e13bd675d2b8c08600dd
sujannepalwsn <sujan1nepal.wsn@gmail.com>
2026-03-05 00:07:45 +0545

    Update HomeworkManagement.tsx

--- src/pages/HomeworkManagement.tsx
+++ src/pages/HomeworkManagement.tsx
@@ -10,7 +10,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogDescription } from "@/components/ui/dialog";
 import { toast } from "sonner";
-import { Book, CheckCircle, Clock, Edit, FileUp, Image, Plus, Trash2, Users, XCircle } from "lucide-react";
+import { Book, CheckCircle, Clock, Edit, FileUp, Image, Loader2, Plus, Trash2, Users, XCircle } from "lucide-react";
 import { format } from "date-fns";
 import { Tables } from "@/integrations/supabase/types";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
